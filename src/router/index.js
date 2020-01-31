84cf3259bb34a8577281106e6df091eef09d38cb
Fabio Vedovelli <fabio.vedovelli@raisin.com>
2020-01-31 07:30:03 +0100

    Init

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import PropertyCard from "../views/PropertyCard.vue";
 
 Vue.use(VueRouter);
 
@@ -8,7 +8,7 @@ const routes = [
   {
     path: "/",
     name: "home",
-    component: Home
+    component: PropertyCard
   },
   {
     path: "/about",
